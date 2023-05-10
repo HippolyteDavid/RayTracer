e2590f25f4785aa9070d4352e3eae9d23008e7a9
Hippolyte DAVID <hippolyte.david@epitech.eu>
2023-05-10 17:03:17 +0200

    [REMOVE] :fire: --> Useless Files

--- build/CMakeFiles/raytracer.dir/src/utils/Color.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/raytracer.dir/src/utils/Color.cpp.o: \
- /home/Hippolyte_D/Work/Tech2/OOP/raytracer/src/utils/Color.cpp \
- /usr/include/stdc-predef.h \
- /home/Hippolyte_D/Work/Tech2/OOP/raytracer/include/material/Color.hpp
--- build/src/plugins/encoder/sfmlEncoder/CMakeFiles/sfmlEncoder.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sfmlEncoder.
-# This may be replaced when dependencies are built.
